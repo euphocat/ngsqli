d5febdfa0f42969f5b53f32a4c682f537c06fe9c
Nicolas Baptiste <nicolas.baptiste@gmail.com>
2016-05-19 15:07:14 +0200

    TP5 TODO

---
+++ TP5_communication_serveur.md
@@ -0,0 +1,20 @@
+# Formation AngularJS
+
+## TP5. Communication serveur
+
+### Durée
+~ 20min
+
+### Objectif
+
+Lister les livres dans la vue home
+
+### TODO
+1. Ajouter la dépendance au service $http dans le controleur HomeCtrl
+2. Utiliser $http.get pour récupérer la liste des livres et initialiser une variable vm.books
+3. Afficher la liste des livres dans la vue associée
+
+### Tips
+Se placer dans le répertoire backend et faire un ```npm install```
+Puis ```node index.js```
+Se rendre à l'adresse http://localhost:3000/back/books pour voir la liste des livres
---
+++ backend/index.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const Hapi = require('hapi');
+const uuid = require('uuid');
+
+let books = [
+  {
+    id: uuid.v4(),
+    title: 'The Lords Of The Rings',
+    isbn: 1234567891112,
+    author: {
+      firstName: 'JRR',
+      lastName: 'Tolkien'
+    },
+    category: 'fantasy,hobbits'
+  },
+  {
+    id: uuid.v4(),
+    title: 'Game Of Thrones',
+    isbn: 1234567891177,
+    author: {
+      firstName: 'George R',
+      lastName: 'Martin'
+    },
+    category: 'fantasy,war'
+  }
+];
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, routes : { cors: true }});
+
+server.start(err => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+  method: 'GET',
+  path: '/back/books',
+  handler: function (request, reply) {
+      reply(books);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/back/books/{id}',
+  handler: function (request, reply) {
+      const id = request.params.id;
+      
+      reply(books.find(book => book.id === id));
+  }
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/back/books/{id}',
+  handler: function (request, reply) {
+      const id = request.params.id;
+      const bookIndex = books.findIndex(book => book.id === id);
+
+      books.splice(bookIndex, 1);
+      reply(true);
+  }
+});
+
+server.route({
+  method: 'PUT',
+  path: '/back/books/{id}',
+  handler: function (request, reply) {
+    const id = request.params.id;
+    const bookIndex = books.findIndex(book => book.id === id);
+
+    books[bookIndex] = request.payload;
+    reply(true);
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/back/books',
+  handler: function (request, reply) {
+    let book = request.payload;
+    book.id = uuid.v4();
+    books.push(book);
+    reply(true);
+  }
+});
