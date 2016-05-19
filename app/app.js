d0df641bcd47515c87d95ed3d48b925fb57d40a5
Nicolas Baptiste <nicolas.baptiste@gmail.com>
2016-05-19 13:35:24 +0200

    TP4 Correction

--- app/app.js
+++ app/app.js
@@ -2,6 +2,23 @@
 
   'use strict';
 
-  angular.module('bibliotheque', ['ui.bootstrap']);
+  angular.module('bibliotheque', ['ui.bootstrap', 'ngMessages'])
+    .controller('EditionCtrl', function () {
+      var vm = this;
+
+      vm.getClassesActive = getClassesActive;
+
+      vm.submitBook = submitBook;
+
+      function submitBook () {
+        console.log(vm.book);
+      }
+
+      function getClassesActive (name) {
+        return {
+          'has-success' : vm.bookEdition[name].$dirty && vm.bookEdition[name].$valid,
+          'has-error': vm.bookEdition[name].$dirty && vm.bookEdition[name].$invalid}
+        }
+    });
 
 })();
