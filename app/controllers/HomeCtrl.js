8bf0190e4200e1c4c969ec8d7ffc25ddc2df976d
Nicolas Baptiste <nicolas.baptiste@gmail.com>
2016-05-19 20:13:15 +0200

    TP5 Correction

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -3,8 +3,15 @@
   'use strict';
 
   angular.module('bibliotheque')
-    .controller('HomeCtrl', function () {
+    .controller('HomeCtrl', ['$http', function ($http) {
 
-    });
+      var vm = this;
+
+      $http.get('http://localhost:3000/back/books')
+        .success(function (data/*, status, headers, config*/) {
+          vm.books = data;
+        });
+
+    }]);
 
 })();
